7bcc1de3b520e3d61c34412c4b80ea033d4f73fd
JoshuaMukherjee <joshuaakmukherjee@gmail.com>
2023-12-01 13:26:22 +0000

    gorkov

--- acoustools/src/acoustools/Optimise/Objectives.py
+++ acoustools/src/acoustools/Optimise/Objectives.py
@@ -3,8 +3,36 @@ Objective Functions to be used in Solver.gradient_descent_solver
 Must have the signature (transducer_phases, points, board, targets, **objective_params) -> loss
 '''
 
-from acoustools.Utilities import propagate_abs
+from acoustools.Utilities import propagate_abs, add_lev_sig
+from acoustools.Gorkov import gorkov_analytical
 import torch
 
 def propagate_abs_sum_objective(transducer_phases, points, board, targets, **objective_params):
-    return torch.sum(propagate_abs(transducer_phases,points,board))
+    return torch.sum(propagate_abs(transducer_phases,points,board),dim=1)
+
+
+def gorkov_analytical_sum_objective(transducer_phases, points, board, targets, **objective_params):
+    transducer_phases = add_lev_sig(transducer_phases)
+    
+    axis = objective_params["axis"] if "axis" in objective_params else "XYZ"
+    U = gorkov_analytical(transducer_phases, points, board, axis)
+
+    return torch.sum(U,dim=1).squeeze_(1)
+
+def gorkov_trapping_stiffness_objective(transducer_phases, points, board, targets, **objective_params):
+    '''
+    Adapted from: \\
+    Hirayama, R., Christopoulos, G., Martinez Plasencia, D., & Subramanian, S. (2022). \\
+    High-speed acoustic holography with arbitrary scattering objects. \\
+    In Sci. Adv (Vol. 8). https://www.science.org
+    '''
+    t2 = add_lev_sig(transducer_phases)
+    axis = objective_params["axis"] if "axis" in objective_params else "XYZ"
+    w = objective_params["w"] if "w" in objective_params else 1e-4
+
+    U = gorkov_analytical(t2, points, board, axis)
+
+    return torch.sum(U + w*(torch.mean(U) - U)**2,dim=1).squeeze_(1)
+
+
+
--- acoustools/tests/test_gradient_solver.py
+++ acoustools/tests/test_gradient_solver.py
@@ -2,14 +2,22 @@
 
 if __name__ == "__main__":
     from acoustools.Solvers import gradient_descent_solver
-    from acoustools.Utilities import create_points, propagate_abs
-    from acoustools.Optimise.Objectives import propagate_abs_sum_objective
+    from acoustools.Utilities import create_points, propagate_abs, add_lev_sig
+    from acoustools.Optimise.Objectives import propagate_abs_sum_objective, gorkov_analytical_sum_objective
     from acoustools.Optimise.Constraints import constrain_phase_only
-        
+    from acoustools.Gorkov import gorkov_analytical
 
     p = create_points(4,2)
 
     x = gradient_descent_solver(p,propagate_abs_sum_objective, 
-                                maximise=True, constrains=constrain_phase_only, log=True, lr=1e-1)
+                                maximise=True, constrains=constrain_phase_only, log=False, lr=1e-1)
 
     print(propagate_abs(x,p))
+
+
+    x2 = gradient_descent_solver(p,gorkov_analytical_sum_objective, constrains=constrain_phase_only,log=False, lr=1e-1)
+
+    print(propagate_abs(x2,p))
+    x2 = add_lev_sig(x2)
+    print(gorkov_analytical(x2,p))
+
