d68ef9cbd8239a99666663911bcd5d74cafaf024
JoshuaMukherjee <joshuaakmukherjee@gmail.com>
2023-12-04 12:43:44 +0000

    comments

--- acoustools/src/acoustools/Constants.py
+++ acoustools/src/acoustools/Constants.py
@@ -1,13 +1,24 @@
 
 pi = 3.1415926535
-R = .001
+R = .001 
+'''Radius of particle'''
 V = 4/3 * pi * R**3
+'''Volume of Particle'''
 c_0 = 343
+'''Speed of sound in air'''
 p_0 = 1.2
+'''Density of air'''
 c_p = 1052
-p_p = 29.36 #From Holographic acoustic elements for manipulation of levitated objects
+'''Speed of sound in EPS particle'''
+p_p = 29.36 
+'''density of EPS particle, From Holographic acoustic elements for manipulation of levitated objects'''
 f = 40000
+'''Frequency of 40KHz sound'''
 wavelength = 0.00865
+'''Wavelength of 40KHz sound'''
 k = 2*pi / wavelength
-radius=0.005
+'''Wavenumber of 40KHz sound'''
+radius=0.005 
+'''Radius of transducer'''
 P_ref = 8.02
+'''Reference pressure for transducers'''
