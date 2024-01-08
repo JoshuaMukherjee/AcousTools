bfb0ef18b7ad2743ebff353c89ba56b6b9a70ad4
JoshuaMukherjee <joshuaakmukherjee@gmail.com>
2024-01-08 18:01:02 +0000

    tests

--- acoustools/tests/test_force_mesh_derivative.py
+++ acoustools/tests/test_force_mesh_derivative.py
@@ -1,25 +1,30 @@
 from acoustools.Gorkov import force_mesh_derivative
 from acoustools.BEM import  get_cache_or_compute_H
-from acoustools.Mesh import load_scatterer,scale_to_diameter, get_centres_as_points, get_normals_as_points, get_areas
+from acoustools.Mesh import load_scatterer,scale_to_diameter, get_centres_as_points, get_normals_as_points, get_areas, load_multiple_scatterers, merge_scatterers
 from acoustools.Utilities import TRANSDUCERS
 from acoustools.Solvers import wgs_wrapper
 
 
 
 if __name__ == "__main__":
+    wall_paths = ["../BEMMedia/flat-lam1.stl","../BEMMedia/flat-lam1.stl"]
+    walls = load_multiple_scatterers(wall_paths,dxs=[-0.06,0.06],rotys=[90,-90], board=TRANSDUCERS) #Make mesh at 0,0,0
+    
     ball_path = "../BEMMedia/Sphere-lam2.stl"
     ball = load_scatterer(ball_path,dy=-0.06) #Make mesh at 0,0,0
     scale_to_diameter(ball,0.04)
     
-    centres = get_centres_as_points(ball)
+    scatterer = merge_scatterers(ball, walls)
 
-    H = get_cache_or_compute_H(ball,TRANSDUCERS,path = "../BEMMedia/")
+    centres = get_centres_as_points(scatterer)
+
+    H = get_cache_or_compute_H(scatterer,TRANSDUCERS,path = "../BEMMedia/")
     x = wgs_wrapper(centres,A=H)
 
-    norms = get_normals_as_points(ball)
-    areas = get_areas(ball)
+    norms = get_normals_as_points(scatterer)
+    areas = get_areas(scatterer)
 
     
-    Fa = force_mesh_derivative(x,centres,norms,areas,TRANSDUCERS,ball)
+    Fa = force_mesh_derivative(x,centres,norms,areas,TRANSDUCERS,scatterer)
 
     print(Fa)
--- acoustools/tests/test_get_force_mesh_along_axis.py
+++ acoustools/tests/test_get_force_mesh_along_axis.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     from acoustools.Gorkov import force_mesh, compute_force, get_force_mesh_along_axis
     from acoustools.Utilities import create_points, propagate_abs, add_lev_sig, TRANSDUCERS
     from acoustools.Solvers import wgs_wrapper
-    from acoustools.Mesh import load_multiple_scatterers, get_normals_as_points, get_centres_as_points, get_areas, get_weight
+    from acoustools.Mesh import load_multiple_scatterers, get_normals_as_points, get_centres_as_points, get_areas, get_weight, load_scatterer, scale_to_diameter, merge_scatterers
     import acoustools.Constants as c 
 
     import vedo, torch
@@ -12,19 +12,36 @@ if __name__ == "__main__":
 
     board = TRANSDUCERS
 
-    paths = ["Sphere-lam1.stl"]
-    scatterer = load_multiple_scatterers(paths,board, root_path="../BEMMedia/",dys=[-0.06])
+    wall_paths = ["../BEMMedia/flat-lam1.stl","../BEMMedia/flat-lam1.stl"]
+    walls = load_multiple_scatterers(wall_paths,dxs=[-0.06,0.06],rotys=[90,-90], board=board) #Make mesh at 0,0,0
+    
+    
+    ball_path = "../BEMMedia/Sphere-lam2.stl"
+    ball = load_scatterer(ball_path,dy=-0.06) #Make mesh at 0,0,0
+    scale_to_diameter(ball,0.04)
 
-    weight = get_weight(scatterer, c.p_p)
+    scatterer = merge_scatterers(ball, walls)
+
+    weight = get_weight(ball, c.p_p)
 
     p = get_centres_as_points(scatterer)
 
     x = wgs_wrapper(p, board=board)
 
-    start = torch.tensor([[-0.06],[0],[0]])
-    end = torch.tensor([[0.06],[0],[0]])
+    start = torch.tensor([[-0.04],[0],[0]])
+    end = torch.tensor([[0.04],[0],[0]])
 
-    Fxs, Fys, Fzs = get_force_mesh_along_axis(start, end, x, scatterer, board)
+    Fxs, Fys, Fzs = get_force_mesh_along_axis(start, end, x, [ball,walls], board, path = "../BEMMedia/")
+    Fxs = [f.cpu().detach().numpy() for f in Fxs]
+    Fys = [f.cpu().detach().numpy() for f in Fys]
+    Fzs = [f.cpu().detach().numpy() - weight for f in Fzs]
 
+
+    plt.subplot(3,1,1)
+    plt.plot(Fxs)
+    plt.subplot(3,1,2)
+    plt.plot(Fys)
+    plt.subplot(3,1,3)
     plt.plot(Fzs)
+    
     plt.show()
