ab9a2b4453441a8fa419d14f31b534c1506c1dca
JoshuaMukherjee <joshuaakmukherjee@gmail.com>
2024-04-16 15:38:39 +0100

    wgs

--- acoustools/tests/test_Levitator.py
+++ acoustools/tests/test_Levitator.py
@@ -1,12 +1,12 @@
 if __name__ == '__main__':
     from acoustools.Levitator import LevitatorController
     from acoustools.Utilities import create_points, add_lev_sig
-    from acoustools.Solvers import wgs_wrapper
+    from acoustools.Solvers import wgs
 
     lev = LevitatorController()
 
     p = create_points(1,1,x=0,y=0,z=0)
-    x = wgs_wrapper(p)
+    x = wgs(p)
     x = add_lev_sig(x)
 
     lev.levitate(x)
