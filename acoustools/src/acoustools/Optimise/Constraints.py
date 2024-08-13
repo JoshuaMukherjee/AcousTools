2f55a84bb8f9e7e112dd73636a00b48fe7236538
Joshua Mukherjee <joshuaakmukherjee@gmail.com>
2024-08-13 18:38:19 +0100

    Optimise Docs

--- acoustools/src/acoustools/Optimise/Constraints.py
+++ acoustools/src/acoustools/Optimise/Constraints.py
@@ -4,27 +4,53 @@ Must have signature phases, **params -> phases
 '''
 
 import torch
+from torch import Tensor
 
-def constrain_phase_only(phases, **params):
+def constrain_phase_only(phases: Tensor, **params) -> Tensor:
+    '''
+    Normalises amplitude to 1
+    :param phase: Phases
+    :return: Hologram
+    '''
     return phases / torch.abs(phases)
 
-def constrant_normalise_amplitude(phases, **params):
+def constrant_normalise_amplitude(phases: Tensor, **params)-> Tensor:
+    '''
+    Constrains by dividing by `torch.max(torch.abs(phases))`
+    :param phase: Phases
+    :return: Hologram
+    '''
     return phases / torch.max(torch.abs(phases))
 
-def constrain_sigmoid_amplitude(phases, **params):
+def constrain_sigmoid_amplitude(phases: Tensor, **params)-> Tensor:
+    '''
+    Constrains by dividing by passing through signmoid
+    :param phase: Phases
+    :return: Hologram
+    '''
     amplitudes = torch.abs(phases)
     norm_holo = phases / amplitudes
     con_amp = 0.5 * torch.sigmoid(amplitudes) + 1/2
     # print(torch.abs(norm_holo * sin_amp))
     return norm_holo * con_amp
 
-def constrain_clamp_amp(phases, **params):
+def constrain_clamp_amp(phases: Tensor, **params)-> Tensor:
+    '''
+    Constrains by dividing by clamping
+    :param phase: Phases
+    :return: Hologram
+    '''
     amplitudes = torch.abs(phases)
     norm_holo = phases / amplitudes
     clamp_amp = torch.clamp(amplitudes,min=0,max=1)
     return norm_holo * clamp_amp
 
-def normalise_amplitude_normal(phases, **params):
+def normalise_amplitude_normal(phases: Tensor, **params)-> Tensor:
+    '''
+    Constrains by dividing by z-score
+    :param phase: Phases
+    :return: Hologram
+    '''
     amplitudes = torch.abs(phases)
     norm_holo = phases / amplitudes
     norm_dist_amp = (amplitudes - torch.min(amplitudes,dim=1,keepdim=True).values) / (torch.max(amplitudes,dim=1,keepdim=True).values - torch.min(amplitudes,dim=1,keepdim=True).values)
--- acoustools/src/acoustools/Optimise/Objectives.py
+++ acoustools/src/acoustools/Optimise/Objectives.py
@@ -7,11 +7,28 @@ from acoustools.Utilities import propagate_abs, add_lev_sig
 from acoustools.Gorkov import gorkov_analytical
 import torch
 
-def propagate_abs_sum_objective(transducer_phases, points, board, targets, **objective_params):
+from torch import Tensor
+
+def propagate_abs_sum_objective(transducer_phases: Tensor, points:Tensor, board:Tensor, targets:Tensor, **objective_params) -> Tensor:
+    '''
+    Sum of the pressure of points
+    :param transducer_phases: Hologram
+    :param points: Points
+    :param board: Transducer board
+    :param target: <Unused>
+    '''
     return torch.sum(propagate_abs(transducer_phases,points,board),dim=1)
 
 
-def gorkov_analytical_sum_objective(transducer_phases, points, board, targets, **objective_params):
+def gorkov_analytical_sum_objective(transducer_phases: Tensor, points:Tensor, board:Tensor, targets:Tensor, **objective_params) -> Tensor:
+    '''
+    Sum of the Gor'kov potential of points
+    :param transducer_phases: Hologram
+    :param points: Points
+    :param board: Transducer board
+    :param target: <Unused>
+    :param axis: axis to compute gor'kov potential in
+    '''
     transducer_phases = add_lev_sig(transducer_phases)
     
     axis = objective_params["axis"] if "axis" in objective_params else "XYZ"
@@ -19,13 +36,30 @@ def gorkov_analytical_sum_objective(transducer_phases, points, board, targets, *
 
     return torch.sum(U,dim=1).squeeze_(1)
 
-def gorkov_analytical_mean_objective(transducer_phases, points, board, targets, **objective_params):
+def gorkov_analytical_mean_objective(transducer_phases: Tensor, points:Tensor, board:Tensor, targets:Tensor, **objective_params) -> Tensor:
+    '''
+    Mean of the Gor'kov potential of points
+    :param transducer_phases: Hologram
+    :param points: Points
+    :param board: Transducer board
+    :param target: <Unused>
+    :param axis: axis to compute gor'kov potential in
+    '''
     
     axis = objective_params["axis"] if "axis" in objective_params else "XYZ"
     U = gorkov_analytical(transducer_phases, points, board, axis)
     return torch.mean(U,dim=1).squeeze_(1)
 
-def gorkov_analytical_std_mean_objective(transducer_phases, points, board, targets, **objective_params):
+def gorkov_analytical_std_mean_objective(transducer_phases: Tensor, points:Tensor, board:Tensor, targets:Tensor, **objective_params) -> Tensor:
+    '''
+    Weighted sum of of standard deviation and mean of Gor'kov potential at points. \n
+    :param transducer_phases: Hologram
+    :param points: Points
+    :param board: Transducer board
+    :param target: <Unused>
+    :param w: relative weight of standard deviation and mean
+    :param axis: axis to compute gor'kov potential in
+    '''
     axis = objective_params["axis"] if "axis" in objective_params else "XYZ"
     U = gorkov_analytical(transducer_phases, points, board, axis)
     
@@ -34,12 +68,18 @@ def gorkov_analytical_std_mean_objective(transducer_phases, points, board, targe
     return torch.mean(U,dim=1).squeeze(1) + w*torch.std(U,dim=1).squeeze(1)
 
 
-def gorkov_trapping_stiffness_objective(transducer_phases, points, board, targets, **objective_params):
+def gorkov_trapping_stiffness_objective(transducer_phases: Tensor, points:Tensor, board:Tensor, targets:Tensor, **objective_params) -> Tensor:
     '''
-    Adapted from: \\
-    Hirayama, R., Christopoulos, G., Martinez Plasencia, D., & Subramanian, S. (2022). \\
-    High-speed acoustic holography with arbitrary scattering objects. \\
-    In Sci. Adv (Vol. 8). https://www.science.org
+    Objective from  Hirayama, R., Christopoulos, G., Martinez Plasencia, D., & Subramanian, S. (2022). \n
+    High-speed acoustic holography with arbitrary scattering objects. \n
+    In Sci. Adv (Vol. 8). https://doi.org/10.1126/sciadv.abn7614
+    :param transducer_phases: Hologram
+    :param points: Points
+    :param board: Transducer board
+    :param target: <Unused>
+    :param w: relative weight of parts of objective
+    :param axis: axis to compute gor'kov potential in
+   
     '''
     t2 = add_lev_sig(transducer_phases)
     axis = objective_params["axis"] if "axis" in objective_params else "XYZ"
@@ -50,7 +90,15 @@ def gorkov_trapping_stiffness_objective(transducer_phases, points, board, target
     return torch.sum(U + w*(torch.mean(U) - U)**2,dim=1).squeeze_(1)
 
 
-def pressure_abs_gorkov_trapping_stiffness_objective(transducer_phases, points, board, targets, **objective_params):
+def pressure_abs_gorkov_trapping_stiffness_objective(transducer_phases: Tensor, points:Tensor, board:Tensor, targets:Tensor, **objective_params) -> Tensor:
+    '''
+    weighted sum of `gorkov_trapping_stiffness_objective` and `propagate_abs_sum_objective`
+    :param transducer_phases: Hologram
+    :param points: Points
+    :param board: Transducer board
+    :param target: <Unused>
+    :param alpha: relative weight of parts of objective
+    '''
     Ul = gorkov_trapping_stiffness_objective(transducer_phases, points, board, targets, **objective_params)
     pl = propagate_abs_sum_objective(transducer_phases, points, board, targets, **objective_params)
 
@@ -58,12 +106,26 @@ def pressure_abs_gorkov_trapping_stiffness_objective(transducer_phases, points, 
 
     return Ul + alpha*pl
 
-def target_pressure_mse_objective(transducer_phases, points, board, targets, **objective_params):
+def target_pressure_mse_objective(transducer_phases: Tensor, points:Tensor, board:Tensor, targets:Tensor, **objective_params) -> Tensor:
+    '''
+    MSE error of target pressure and true pressure
+    :param transducer_phases: Hologram
+    :param points: Points
+    :param board: Transducer board
+    :param target:target pressure
+    '''
     p = propagate_abs(transducer_phases, points,board=board)
     l = torch.sum((p-targets)**2,dim=1)
     return l
 
-def target_gorkov_mse_objective(transducer_phases, points, board, targets, **objective_params):
+def target_gorkov_mse_objective(transducer_phases: Tensor, points:Tensor, board:Tensor, targets:Tensor, **objective_params) -> Tensor:
+    '''
+    MSE error of target Gor'kov potential and true Gor'kov potential
+    :param transducer_phases: Hologram
+    :param points: Points
+    :param board: Transducer board
+    :param target: target gor'kov value
+    '''
     if "no_sig" not in objective_params:
         t2 = add_lev_sig(transducer_phases)
     else:
