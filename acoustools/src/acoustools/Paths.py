f02927955a8d28bcba7fc5fb9546d1459f0b5240
Joshua Mukherjee <joshuaakmukherjee@gmail.com>
2024-08-13 16:47:08 +0100

    Paths doc

--- acoustools/src/acoustools/Paths.py
+++ acoustools/src/acoustools/Paths.py
@@ -1,7 +1,18 @@
 import torch
 import itertools
 
-def get_numeral(numeral, A, B, C):
+from torch import Tensor
+
+def get_numeral(numeral:int|str, A:Tensor, B:Tensor, C:Tensor) -> list[Tensor]:
+    '''
+    Get positions to draw a given numeral \n
+    :param numeral: The number to generate a path for [1,9] 
+    :param A: top left corner of the box to draw in
+    :param B: bottom left corner of the box to draw in
+    :param C: top right corner of the box to draw in
+    :return: path
+
+    '''
     if type(A) is not torch.Tensor or type(B) is not torch.Tensor or type(C) is not torch.Tensor:
         A = torch.Tensor(A)
         B = torch.Tensor(B)
@@ -26,6 +37,9 @@ def get_numeral(numeral, A, B, C):
         return numeral_nine(A,B,C)
 
 def numeral_one(A,B,C):
+    '''
+    @private
+    '''
     AB = B-A
     AC = C-A
     
@@ -37,6 +51,9 @@ def numeral_one(A,B,C):
     return points
 
 def numeral_two(A,B,C):
+    '''
+    @private
+    '''
     AB = B-A
     AC = C-A
     
@@ -50,6 +67,9 @@ def numeral_two(A,B,C):
     return points
 
 def numeral_three(A,B,C):
+    '''
+    @private
+    '''
     AB = B-A
     AC = C-A
     
@@ -64,6 +84,9 @@ def numeral_three(A,B,C):
     return points
 
 def numeral_four(A,B,C):
+    '''
+    @private
+    '''
     AB = B-A
     AC = C-A
     
@@ -79,6 +102,9 @@ def numeral_four(A,B,C):
     return points
 
 def numeral_five(A,B,C):
+    '''
+    @private
+    '''
     AB = B-A
     AC = C-A
     
@@ -93,6 +119,9 @@ def numeral_five(A,B,C):
     return points
 
 def numeral_six(A,B,C):
+    '''
+    @private
+    '''
     AB = B-A
     AC = C-A
 
@@ -107,6 +136,9 @@ def numeral_six(A,B,C):
     return points
 
 def numeral_seven(A,B,C):
+    '''
+    @private
+    '''
     AB = B-A
     AC = C-A
 
@@ -118,6 +150,9 @@ def numeral_seven(A,B,C):
     return points
 
 def numeral_eight(A,B,C):
+    '''
+    @private
+    '''
     AB = B-A
     AC = C-A
 
@@ -132,6 +167,9 @@ def numeral_eight(A,B,C):
     return points
 
 def numeral_nine(A,B,C):
+    '''
+    @private
+    '''
     AB = B-A
     AC = C-A
 
@@ -145,10 +183,23 @@ def numeral_nine(A,B,C):
     return points
 
 
-def distance(p1, p2):
+def distance(p1:Tensor, p2:Tensor) -> float:
+    '''
+    Computes the euclidian distance between two points
+    :param p1: First point
+    :param p2: Second point
+    :return: Distance
+    '''
     return torch.sqrt(torch.sum((p2 - p1)**2))
     
-def interpolate_points(p1, p2, n):
+def interpolate_points(p1:Tensor, p2:Tensor, n:int)-> list[Tensor]:
+    '''
+    Interpolates `n` points between `p1` and `p2`
+    :param p1: First point
+    :param p2: Second point
+    :param n: number of points to interpolate
+    :return: Path
+    '''
     vec = (p2 - p1) / n
     points = []
     for i in range(n):
@@ -156,7 +207,13 @@ def interpolate_points(p1, p2, n):
 
     return points
 
-def interpolate_path(path, n, return_distance = False):
+def interpolate_path(path: list[Tensor], n:int, return_distance:bool = False) -> list[Tensor]:
+    '''
+    Calls `interpolate_points on all adjacent pairs of points in path`
+    :param n: TOTAL number of points to interpolate (will be split between pairs)
+    :param return_distance: if `True` will also return total distance
+    :return: Path and optionally total distance
+    '''
     points = []
     total_dist = 0
     distances = []
