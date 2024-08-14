3bb25919250391e5fc9812fd064239d79676b775
Joshua Mukherjee <joshuaakmukherjee@gmail.com>
2024-08-14 13:53:29 +0100

    code example

--- acoustools/src/acoustools/BEM.py
+++ acoustools/src/acoustools/BEM.py
@@ -467,34 +467,28 @@ def compute_E(scatterer:Mesh, points:Tensor, board:Tensor|None=None, use_cache_H
     :return E: Propagation matrix for BEM E
 
     ```Python
-            from acoustools.Mesh import load_scatterer
-            from acoustools.BEM import compute_E, propagate_BEM_pressure, compute_H
-            from acoustools.Utilities import create_points, TOP_BOARD
-            from acoustools.Solvers import wgs
-            from acoustools.Visualiser import Visualise
+    from acoustools.Mesh import load_scatterer
+    from acoustools.BEM import compute_E, propagate_BEM_pressure, compute_H
+    from acoustools.Utilities import create_points, TOP_BOARD
+    from acoustools.Solvers import wgs
+    from acoustools.Visualiser import Visualise
 
-            import torch, vedo
+    import torch
 
-            path = "../../BEMMedia"
-            scatterer = load_scatterer(path+"/Sphere-lam2.stl",dy=-0.06,dz=-0.08)
-            
-            N=1
-            B=1
-            p = create_points(N,B,y=0,x=0,z=0)
-            
-            H = compute_H(scatterer, TOP_BOARD)
-            E, F, G, H = compute_E(scatterer, p, TOP_BOARD,path=path,use_cache_H=False,return_components=True,H=H)
-            x = wgs(p,board=TOP_BOARD,A=E)
-            
-            A = torch.tensor((-0.12,0, 0.12))
-            B = torch.tensor((0.12,0, 0.12))
-            C = torch.tensor((-0.12,0, -0.12))
-            normal = (0,1,0)
-            origin = (0,0,0)
+    path = "../../BEMMedia"
+    scatterer = load_scatterer(path+"/Sphere-lam2.stl",dy=-0.06,dz=-0.08)
+    
+    p = create_points(N=1,B=1,y=0,x=0,z=0)
+    
+    H = compute_H(scatterer, TOP_BOARD)
+    E = compute_E(scatterer, p, TOP_BOARD,path=path,H=H)
+    x = wgs(p,board=TOP_BOARD,A=E)
+    
+    A = torch.tensor((-0.12,0, 0.12))
+    B = torch.tensor((0.12,0, 0.12))
+    C = torch.tensor((-0.12,0, -0.12))
 
-            line_params = {"scatterer":scatterer,"origin":origin,"normal":normal}
-
-            Visualise(A,B,C, x, colour_functions=[propagate_BEM_pressure],colour_function_args=[{"scatterer":scatterer,"board":TOP_BOARD,"path":path,'H':H}],vmax=8621, show=True,res=[256,256])
+    Visualise(A,B,C, x, colour_functions=[propagate_BEM_pressure],colour_function_args=[{"scatterer":scatterer,"board":TOP_BOARD,"path":path,'H':H}],vmax=8621, show=True,res=[256,256])
     ```
     
     '''
