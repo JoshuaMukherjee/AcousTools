c7c83a258b6d0e31ca9095441bd5a8f79b642857
Abdullah Saeed <abdullahsaeed91203@gmail.com>
2025-03-15 17:07:34 +0000

    some edits

--- BEM_trap_stiffness.py
+++ BEM_trap_stiffness.py
@@ -5,8 +5,6 @@ import torch
 from torch import Tensor
 from vedo import Mesh
 
-#use compute stiffness code but replace compute_force with my code
-
 def BEM_trap_stiffness(activations: Tensor, points: Tensor, scatterer: Mesh|None|str=None,
                        board: Tensor|None=None, H: Tensor|None=None, E: Tensor|None=None, 
                        delta: float=0.001, dims: str='XYZ', **params)  -> Tensor:
