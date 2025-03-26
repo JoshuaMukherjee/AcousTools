1d5b104f899cb1146b2897f76948a3bfe062e271
Joshua Mukherjee <joshuaakmukherjee@gmail.com>
2025-03-26 17:50:56 +0000

    holo file

--- acoustools/src/acoustools/Export/Holo.py
+++ acoustools/src/acoustools/Export/Holo.py
@@ -1,8 +1,11 @@
 '''
-Export to .holo file -> List of holograms
+Export to .holo file -> List of holograms\n
+The holo format stores each phase and amplitude as a 5 bit integer with each phase and ampplitude being discritised to 32 levels \n
+The result of this is then compressed using pythons zlib (https://docs.python.org/3/library/zlib.html)
+Internally stored as blocks of phase and amplitude seperated by 2^6-1 eg phase1 <11111> amplitude1 <11111> phase2 <11111> amplitude2 <11111> ...
 '''
 
-import pickle, torch
+import pickle, torch, io, zlib
 from acoustools.Utilities.Utilities import batch_list
 from acoustools.Utilities.Setup import device,DTYPE
 # from acoustools.Constants import wavelength
@@ -43,7 +46,7 @@ def decompress(phases:list, amplitudes:list, levels:int=32) -> list[Tensor]:
 
 def save_holograms(holos:list[Tensor]|Tensor, fname:str):
     '''
-    Save holograms in .holo format. The holo format stores each phase and amplitude as a 6 bit integer with each phase and ampplitude being discritised to 32 levels
+    Save holograms in .holo format. 
     :param holos: Holograms to use
     :param fname:  filename to use. Will append .holo is no extension provides
     '''
@@ -51,17 +54,23 @@ def save_holograms(holos:list[Tensor]|Tensor, fname:str):
         fname += '.holo'
     # pickle.dump(holos, open(fname, 'wb'))
     
-    with open(fname, 'wb') as file:
+    # with open(fname, 'wb') as file:
+    with io.BytesIO() as file:
         for holo in holos:
             phase, amp = compress(holo.squeeze())
             for p in phase:
                 p = int(p.item())
-                file.write((p).to_bytes(6, byteorder='big', signed=False))
-            file.write((2**6).to_bytes(6, byteorder='big', signed=False))
+                file.write((p).to_bytes(5, byteorder='big', signed=False))
+            file.write((2**6-1).to_bytes(5, byteorder='big', signed=False))
             for a in amp:
                 a = int(a.item())
-                file.write((a).to_bytes(6, byteorder='big', signed=False))
-            file.write((2**6).to_bytes(6, byteorder='big', signed=False))
+                file.write((a).to_bytes(5, byteorder='big', signed=False))
+            file.write((2**6-1).to_bytes(5, byteorder='big', signed=False))
+        
+        new_data = zlib.compress(file.getbuffer())
+        f = open(fname, 'wb')
+        f.write(new_data)
+        f.close()
 
 def load_holograms(path:str) -> list[Tensor]:
     '''
@@ -79,9 +88,10 @@ def load_holograms(path:str) -> list[Tensor]:
         holos = []
         reading_amps = 0
         data = file.read()
-        for bits in batch_list(data,6):
+        data = zlib.decompress(data)
+        for bits in batch_list(data,5):
             j = int.from_bytes(bits, byteorder='big')
-            if j < 2**6:
+            if j < 2**6-1:
                 if not reading_amps:
                     phases.append(j)
                 else:
