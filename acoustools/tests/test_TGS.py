f50f83067475339351ece22ebd14229162c30a73
Pengyuan Wei <1527755386@qq.com>
2025-03-31 16:41:35 +0100

    Revised the function names of the imported solvers and ensured all variables are on the CUDA device.

--- acoustools/tests/test_TGS.py
+++ acoustools/tests/test_TGS.py
@@ -2,7 +2,7 @@
 if __name__ == '__main__':
 
     from acoustools.Utilities import create_points,forward_model_batched,TRANSDUCERS, device, add_lev_sig
-    from acoustools.Solvers import naive_solver_batch, wgs_batch, temporal_wgs
+    from acoustools.Solvers import naive_solver_batched, wgs_solver_batch, temporal_wgs
     from acoustools.Visualiser import Visualise
 
     import torch
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     p = create_points(N,1,y=0)
     F = forward_model_batched(p,TRANSDUCERS)
 
-    _,_,x_wgs = wgs_batch(F, torch.ones(N,1).to(device)+0j,200)
+    _,_,x_wgs = wgs_solver_batch(F, torch.ones(N,1).to(device)+0j,200)
     x_wgs = add_lev_sig(x_wgs)
 
 
@@ -24,12 +24,10 @@ if __name__ == '__main__':
     _,_,x = temporal_wgs(F,torch.ones(N,1).to(device)+0j,200, x_wgs, F@x_wgs, T_in, T_out)
     # x = add_lev_sig(x)
 
-    A = torch.tensor((-0.09,0, 0.09))
-    B = torch.tensor((0.09,0, 0.09))
-    C = torch.tensor((-0.09,0, -0.09))
+    A = torch.tensor((-0.09, 0, 0.09), device=device)
+    B = torch.tensor((0.09, 0, 0.09), device=device)
+    C = torch.tensor((-0.09, 0, -0.09), device=device)
     normal = (0,1,0)
     origin = (0,0,0)
-    
 
     Visualise(A,B,C, x, points=p,vmax=5000)
-    
