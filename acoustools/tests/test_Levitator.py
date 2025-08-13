97840b830dac86292b55e9e09587d3b22a11161d
Joshua Mukherjee <joshuaakmukherjee@gmail.com>
2025-08-13 17:37:02 +0100

    ids

--- acoustools/tests/test_Levitator.py
+++ acoustools/tests/test_Levitator.py
@@ -3,10 +3,11 @@ if __name__ == '__main__':
     from acoustools.Utilities import create_points, add_lev_sig, propagate_abs
     from acoustools.Solvers import wgs
 
-    lev = LevitatorController(ids=(73,53))
+    lev = LevitatorController(ids=(1000,999))
 
     p = create_points(1,1,x=0,y=0,z=0)
     x = wgs(p)
+
     print(propagate_abs(x,p))
     print(x.shape)
     x = add_lev_sig(x)
