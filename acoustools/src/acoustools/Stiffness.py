41bb7d981efcf3301dc77879e168bbb7ffbc30e6
JoshuaMukherjee <joshuaakmukherjee@gmail.com>
2025-10-22 16:57:28 +0100

    fix

--- acoustools/src/acoustools/Stiffness.py
+++ acoustools/src/acoustools/Stiffness.py
@@ -4,7 +4,7 @@ from acoustools.Utilities import create_points, TRANSDUCERS
 
 from torch import Tensor
 
-def stiffness_finite_differences(activation:Tensor, points:Tensor, board:Tensor|None=None, delta= 0.001):
+def stiffness_finite_differences(activations:Tensor, points:Tensor, board:Tensor|None=None, delta= 0.001):
     '''
     Computes the stiffness at a point as the gradient of the force. Force computed analytically and then finite differences used to find the gradient \n
     Computed as `-1* (Fx + Fy + Fz)` where `Fa` is the gradient of force in that direction \n 
@@ -23,19 +23,18 @@ def stiffness_finite_differences(activation:Tensor, points:Tensor, board:Tensor|
     dz = create_points(1,1,0,0,delta)
 
 
-
-    Fx1 = compute_force(activation,points + dx,board=board)[0]
-    Fx2 = compute_force(activation,points - dx,board=board)[0]
+    Fx1 = compute_force(activations,points + dx,board=board)[:,:,0]
+    Fx2 = compute_force(activations,points - dx,board=board)[:,:,0]
 
     Fx = ((Fx1 - Fx2) / (2*delta))
 
-    Fy1 = compute_force(activation,points + dy,board=board)[1]
-    Fy2 = compute_force(activation,points - dy,board=board)[1]
+    Fy1 = compute_force(activations,points + dy,board=board)[:,:,1]
+    Fy2 = compute_force(activations,points - dy,board=board)[:,:,1]
 
     Fy = ((Fy1 - Fy2) / (2*delta))
 
-    Fz1 = compute_force(activation,points + dz,board=board)[2]
-    Fz2 = compute_force(activation,points - dz,board=board)[2]
+    Fz1 = compute_force(activations,points + dz,board=board)[:,:,2]
+    Fz2 = compute_force(activations,points - dz,board=board)[:,:,2]
     
     Fz = ((Fz1 - Fz2) / (2*delta))
 
